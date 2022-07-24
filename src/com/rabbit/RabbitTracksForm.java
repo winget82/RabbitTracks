aaf3a6163a0277e88048d58aa37b02cf3c3176b1
Nate Flesher <nflesher82@yahoo.com>
2022-07-24 15:52:31 -0500

    Started work on GUI layout

--- src/com/rabbit/RabbitTracksForm.java
+++ src/com/rabbit/RabbitTracksForm.java
@@ -3,8 +3,31 @@ package com.rabbit;
 import javax.swing.*;
 
 public class RabbitTracksForm {
-    private JTabbedPane tabbedPane1;
-    private JPanel panel1;
+    private JTabbedPane RabbitTrakcsTabbedPane;
+    private JPanel MainPanel;
+    private JPanel EnterRabbitTab;
+    private JPanel EnterLitterTab;
+    private JPanel HarvestStatsTab;
+    private JPanel BreedingStatsTab;
+    private JPanel RabbitStockTab;
+    private JTextField textField1;
+    private JRadioButton maleRadioButton;
+    private JRadioButton femaleRadioButton;
+    private JTextField textField3;
+    private JTextArea textArea1;
+    private JTextField textField2;
+    private JTextField textField4;
+    private JTextField textField5;
+    private JButton cancelButton;
+    private JButton OKButton;
+
+    public static void main(String[] args) {
+        JFrame frame = new JFrame("RabbitTracksForm");
+        frame.setContentPane(new RabbitTracksForm().MainPanel);
+        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        frame.pack();
+        frame.setVisible(true);
+    }
 }
 
 //form to read from and save to sqlite database all rabbit breeding / harvesting info
